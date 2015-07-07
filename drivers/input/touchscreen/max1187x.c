79a8e45ec849491be047a33b4c5908737ed71403
Tommy-Geenexus <roboter972@gmail.com>
2015-07-07 09:14:27 +0200

    input: max1187x: disable wakeup_gesture

--- drivers/input/touchscreen/max1187x.c
+++ drivers/input/touchscreen/max1187x.c
@@ -66,7 +66,10 @@
 		INFO_BUFFER(DEV, BUF, LEN, FMT, FLN);	\
 })
 
+#if 0
+// Tommy-Geenexus
 #define MXM_TOUCH_WAKEUP_FEATURE
+#endif
 
 #define MXM_CMD_LEN_PACKET_MAX      9
 #define MXM_CMD_LEN_MAX             (15 * MXM_CMD_LEN_PACKET_MAX)
