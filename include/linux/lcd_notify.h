53f8d84868d401cef9a412a423d7de1c91a72abc
Devin Kim <dojip.kim@lge.com>
2017-07-19 21:56:46 +0200

    msm: mdss: Adding lcd notifier Supports notifier for start&end of LCD power on&off

--- include/linux/lcd_notify.h
+++ include/linux/lcd_notify.h
@@ -1,6 +1,8 @@
 #ifndef __LINUX_LCD_NOTIFY_H
 #define __LINUX_LCD_NOTIFY_H
+
 #include <linux/notifier.h>
+
 /* the display on process started */
 #define LCD_EVENT_ON_START		0x01
 /* the display on process end */
@@ -9,9 +11,11 @@
 #define LCD_EVENT_OFF_START		0x03
 /* the display off process end */
 #define LCD_EVENT_OFF_END		0x04
+
 struct lcd_event {
 	void *data;
 };
+
 #ifdef CONFIG_FB_MSM_MDSS
 int lcd_register_client(struct notifier_block *nb);
 int lcd_unregister_client(struct notifier_block *nb);
