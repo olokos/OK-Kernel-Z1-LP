5ab1f4359e3415c1c68703c770492674f324ba74
Russ Knize <Russ.Knize@motorola.com>
2017-07-20 15:34:50 +0200

    mmc: Sandisk eMMC quirks Certain Sandisk iNAND devices have a race condition during an HPI that causes the card to enter the incorrect state.  Add a small delay between the STOP and HPI to make sure the card doesn't miss the HPI.  Make sure that, during the post-HPI mop-up process on a write, we try a little harder to determine how many sectors were written.  If the register read fails, double-check the card state, re-send a STOP if needed, and try again.
    Also, remove an old Sandisk quirk that doesn't apply.

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -365,6 +365,7 @@ struct mmc_card {
 #define MMC_QUIRK_BROKEN_DATA_TIMEOUT	(1<<12)
 
 #define MMC_QUIRK_CACHE_DISABLE (1 << 14)       /* prevent cache enable */
+#define MMC_QUIRK_SLOW_HPI_RESPONSE (1 << 30)   /* wait between STOP and HPI */
 
 	unsigned int		erase_size;	/* erase size in sectors */
  	unsigned int		erase_shift;	/* if erase unit is power 2 */
@@ -462,6 +463,7 @@ struct mmc_fixup {
 #define CID_MANFID_TOSHIBA	0x11
 #define CID_MANFID_MICRON	0x13
 #define CID_MANFID_SAMSUNG	0x15
+#define CID_MANFID_SANDISK2	0x45
 #define CID_MANFID_KINGSTON	0x70
 #define CID_MANFID_HYNIX	0x90
 
